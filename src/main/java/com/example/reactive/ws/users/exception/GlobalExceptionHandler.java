450ed0d07352cbef3b55bd0c34f0be2144f39acf
Subrat <skbehera@live.in>
2025-11-11 23:48:51 +0530

    Added Exception Handler for Bean Validation errors

--- src/main/java/com/example/reactive/ws/users/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/reactive/ws/users/exception/GlobalExceptionHandler.java
@@ -3,10 +3,14 @@ package com.example.reactive.ws.users.exception;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,6 +26,16 @@ public class GlobalExceptionHandler {
     public Mono<ErrorResponse> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
         final var errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST,
                 exception.getMessage()).build();
+        return Mono.just(errorResponse);
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public Mono<ErrorResponse> handleWebExchangeBindException(WebExchangeBindException exception) {
+        final var errorMessages = exception.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        final var errorResponse = ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, errorMessages)
+                .build();
         return Mono.just(errorResponse);
     }
 
